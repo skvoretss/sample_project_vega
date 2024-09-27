440353c42009aa86064525c8798a3a63a1f43bf1
Alina <alinaskvorcova200@gmail.com>
2024-09-28 00:39:07 +0300

    Doxygen and CI/CD added

--- my_add.hpp
+++ my_add.hpp
@@ -1,4 +1,11 @@
 #pragma once 
+/**
+    \brief Mathematical additing function
 
+    \param [in] a The first integer summand
+    \param [in] b The second integer summand
+
+    \return New obeject with type *int*, equal to the sum of *a* and *b*.
+*/
 int my_add(int, int);
 
