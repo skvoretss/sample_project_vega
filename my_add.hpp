b815a378de879c472eb9badd3758fe77db9dbab3
skvoretz <alina.skvorcova.2001@yandex.ru>
2024-09-28 17:43:56 +0300

    adding \file to comment

--- my_add.hpp
+++ my_add.hpp
@@ -1,11 +1,14 @@
 #pragma once 
 /**
-    \brief Mathematical additing function
-
-    \param [in] a The first integer summand
-    \param [in] b The second integer summand
-
-    \return New obeject with type *int*, equal to the sum of *a* and *b*.
+ * 
+ * 
+ *   \file my_add.cpp
+ *   \brief Mathematical additing function
+ *
+ *  \param [in] a The first integer summand
+ *  \param [in] b The second integer summand
+ *
+ *  \return New obeject with type *int*, equal to the sum of *a* and *b*.
 */
 int my_add(int, int);
 
